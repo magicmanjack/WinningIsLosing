42ce567313dfbbe8237c61ad13da4758ec5b371e
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2019-01-23 10:41:05 +1300

    Added closing window with rendering.

--- game.cpp
+++ game.cpp
@@ -1,11 +1,16 @@
 #include<SDL.h>
 #include<iostream>
 
-const int WIN_WIDTH = 800, WIN_HEIGHT = 600;
+const int WIN_WIDTH = 800, WIN_HEIGHT = 600, FPS = 60;
+bool running;
+
+void draw(SDL_Renderer * r);
+void update();
 
 int main(int argc, char * args[]) {
 
 	SDL_Window * win = NULL;
+	SDL_Renderer * rend = NULL;
 	
 	SDL_Init(SDL_INIT_VIDEO);
 	
@@ -14,6 +19,62 @@ int main(int argc, char * args[]) {
 	SDL_WINDOWPOS_CENTERED, 
 	WIN_WIDTH, WIN_HEIGHT, 0);
 	
+	rend = SDL_CreateRenderer(win, -1, 0);
+	
+	running = true;
+	double timeNow = SDL_GetTicks(), timeThen = timeNow, ticksPerUpdate = 1000.0d / FPS; // Used for timing.
+	double delta = 0; // How many updates are needed.
+	bool canRender = false;
+	Uint32 timeOut = SDL_GetTicks() + 1000;
+	int updates = 0, frames = 0; // Keeps track of the updates and framerate.
+	
+	while(running) {
+		timeNow = SDL_GetTicks();
+		delta += (timeNow - timeThen) / ticksPerUpdate;
+		timeThen = timeNow;
+		
+		while(delta >= 1) {
+			delta--;
+			update();
+			canRender = true;
+			updates++;
+			
+			SDL_Event event;
+			while(SDL_PollEvent(&event)) {
+				if(event.type == SDL_WINDOWEVENT) {
+					if(event.window.event == SDL_WINDOWEVENT_CLOSE) {
+						running = false; // Exits game loop and closes application.
+					}
+				}
+			}
+		}
+		
+		if(canRender) {
+			SDL_SetRenderDrawColor(rend, 0, 0, 0, SDL_ALPHA_OPAQUE); // Sets background to black.
+			SDL_RenderClear(rend);
+			draw(rend);
+			SDL_RenderPresent(rend);
+			canRender = false;
+			frames++;
+		}
+		
+		if(SDL_TICKS_PASSED(SDL_GetTicks(), timeOut)) {
+			timeOut += 4000;
+			SDL_Log("UPD: %d, FRM: %d", updates / 4, frames / 4); // Logs the update and framerate info.
+			updates = 0;
+			frames = 0;
+		}
+		
+	}
+	
 	SDL_Quit();
 	return 0;
+}
+
+void update() {
+	
+}
+
+void draw(SDL_Renderer * r) {
+	
 }
