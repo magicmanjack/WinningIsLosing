4f9bae5ba0a3c4ddafc42913390089eaad6fbc6b
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2019-01-23 12:30:53 +1300

    Rendered texture

--- game.cpp
+++ game.cpp
@@ -4,8 +4,15 @@
 const int WIN_WIDTH = 800, WIN_HEIGHT = 600, FPS = 60;
 bool running;
 
+bool left, right, up, down;
+
 void draw(SDL_Renderer * r);
 void update();
+void manageInput();
+
+SDL_Rect testRect;
+SDL_Texture * playerTx;
+int speed = 5;
 
 int main(int argc, char * args[]) {
 
@@ -20,6 +27,16 @@ int main(int argc, char * args[]) {
 	WIN_WIDTH, WIN_HEIGHT, 0);
 	
 	rend = SDL_CreateRenderer(win, -1, 0);
+	
+	SDL_Surface * sfce = SDL_LoadBMP("res\\Sprite.bmp");
+	playerTx = SDL_CreateTextureFromSurface(rend, sfce);
+	SDL_FreeSurface(sfce);
+	
+	
+	testRect.x = 350;
+	testRect.y = 250;
+	testRect.w = 100;
+	testRect.h = 100;
 	
 	running = true;
 	double timeNow = SDL_GetTicks(), timeThen = timeNow, ticksPerUpdate = 1000.0d / FPS; // Used for timing.
@@ -38,15 +55,6 @@ int main(int argc, char * args[]) {
 			update();
 			canRender = true;
 			updates++;
-			
-			SDL_Event event;
-			while(SDL_PollEvent(&event)) {
-				if(event.type == SDL_WINDOWEVENT) {
-					if(event.window.event == SDL_WINDOWEVENT_CLOSE) {
-						running = false; // Exits game loop and closes application.
-					}
-				}
-			}
 		}
 		
 		if(canRender) {
@@ -72,9 +80,62 @@ int main(int argc, char * args[]) {
 }
 
 void update() {
-	
+	manageInput();
+	if(left) {
+		testRect.x -= speed;
+	}
+	if(right) {
+		testRect.x += speed;
+	}
+	if(up) {
+		testRect.y -= speed;
+	}
+	if(down) {
+		testRect.y += speed;
+	}
+}
+
+void manageInput() {
+	SDL_Event event;
+	while(SDL_PollEvent(&event)) {
+		if(event.type == SDL_WINDOWEVENT) {
+			if(event.window.event == SDL_WINDOWEVENT_CLOSE) {
+				running = false; // Exits game loop and closes application.
+			}
+		}
+		if(event.type == SDL_KEYDOWN) {
+			if(event.key.keysym.sym == SDLK_a) {
+				left = true;
+			}
+			if(event.key.keysym.sym  == SDLK_d) {
+				right = true;
+			}
+			if(event.key.keysym.sym == SDLK_w) {
+				up = true;
+			}
+			if(event.key.keysym.sym == SDLK_s) {
+				down = true;
+			}
+		}
+		if(event.type == SDL_KEYUP) {
+			if(event.key.keysym.sym == SDLK_a) {
+				left = false;
+			}
+			if(event.key.keysym.sym  == SDLK_d) {
+				right = false;
+			}
+			if(event.key.keysym.sym == SDLK_w) {
+				up = false;
+			}
+			if(event.key.keysym.sym == SDLK_s) {
+				down = false;
+			}
+		}
+	}
 }
 
 void draw(SDL_Renderer * r) {
-	
+	SDL_SetRenderDrawColor(r, 0, 255, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderFillRect(r, &testRect);
+	SDL_RenderCopy(r, playerTx, NULL, &testRect);
 }
